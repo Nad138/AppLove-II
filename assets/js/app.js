111cc6d52a21bee81d79a5c3b857425dd3b5ca0b
Nadia Cuadros <november138@hotmail.com>
2017-04-11 14:45:31 -0500

    adecuando el estilo al html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 function create(){
-  var body = document.getElementById("body");
+  /*var body = document.getElementById("body");
   var section = document.createElement("section");
   var container = document.createElement("div");
   var titulo = document.createElement("h2");
@@ -8,20 +8,37 @@ function create(){
   var img = document.createElement("img");
   var subtitle = document.createElement("h5");
   body.appendChild(section);
+  section.setAttribute("id","work");
   section.appendChild(container);
   container.classList.add("container");
   container.appendChild(titulo);
   titulo.classList.add("text-uppercase");
   titulo.classList.add("text-services");
   titulo.classList.add("mt-0");
-  container.netxtElementSibling="Work";
+  titulo.innerHTML = "Work";
   container.appendChild(texto);
+  texto.innerHTML = "Lorem   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga reprehenderit eligendi adipisci," + "<br>"  +
+    "esse accusantium provident sint tenetur, laborum commdi ad, eius dolore numqui.";
   container.appendChild(div);
-  div.classList.add("box-work");
+  div.classList.add("container");
   div.appendChild(img);
   img.setAttribute("src","assets/img/hero.png");
-
-
-   console.log("hola");
+  img.setAttribute("width","1000");*/
+ console.log("hola");
 }
- create();
+
+function addImage(){
+  var content = document.getElementById("content-box");
+    for(var i=1; i<13;i++){
+        var div = document.createElement("div");
+        div.classList.add("box-work");
+        var image = document.createElement("img");
+        image.setAttribute("src","assets/img/img-"+i+".jpg");
+        image.setAttribute("width","500");
+        var span = document.createElement("span");
+        div.appendChild(image);
+        div.appendChild(span);
+        content.appendChild(div);
+ }
+}
+addImage();
